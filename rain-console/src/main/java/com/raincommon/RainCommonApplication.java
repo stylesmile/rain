4790b1f53fab9c4a38bd26295e63f39d3697905b
chenye <1403539106@qq.com>
2019-05-09 06:47:40 +0800

    init

---
+++ rain-console/src/main/java/com/raincommon/RainCommonApplication.java
@@ -0,0 +1,13 @@
+package com.raincommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RainCommonApplication.class, args);
+    }
+
+}
