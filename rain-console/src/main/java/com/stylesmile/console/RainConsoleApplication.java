628a8774316ac1bb6f86ce33bfc58f7bc7260eac
chenye <3239866994@qq.com>
2019-06-13 23:14:53 +0800

    简单jpa案例

--- rain-console/src/main/java/com/stylesmile/console/RainConsoleApplication.java
+++ rain-console/src/main/java/com/stylesmile/console/RainConsoleApplication.java
@@ -3,6 +3,10 @@ package com.stylesmile.console;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chenye
+ * @date 2019-06-12
+ */
 @SpringBootApplication
 public class RainConsoleApplication {
 
---
+++ rain-console/src/main/java/com/stylesmile/console/repository/BaseJpaRepository.java
@@ -0,0 +1,19 @@
+package com.example.bootjpa.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.io.Serializable;
+
+/**
+ * 基础dao层接口
+ *
+ * @author chenye
+ * @date 2019-06-12
+ */
+@NoRepositoryBean
+public interface BaseJpaRepository<T, ID extends Serializable>
+        extends JpaRepository<T, ID>, JpaSpecificationExecutor<T> {
+
+}
