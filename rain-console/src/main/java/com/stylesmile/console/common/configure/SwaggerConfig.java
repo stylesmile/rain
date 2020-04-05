90590c41f817cfa7f1fc91a2bf64cf3e02236d6b
chenye <15002770045@139.com>
2020-04-05 22:04:27 +0800

    SwaggerConfig移动到common包

--- rain-console/src/main/java/com/stylesmile/console/common/configure/SwaggerConfig.java
+++ rain-console/src/main/java/com/stylesmile/console/common/configure/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.stylesmile.console.configure;
+package com.stylesmile.console.common.configure;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
